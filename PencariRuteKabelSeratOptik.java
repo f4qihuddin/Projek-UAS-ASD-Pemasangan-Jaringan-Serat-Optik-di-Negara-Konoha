630f363a3c1b3ec01a025494b2c2e910c602b26e
Faqihuddin Al-Ghiffary <148179530+f4qihuddin@users.noreply.github.com>
2024-06-08 23:35:47 +0700

    membuat class vertex

--- PencariRuteKabelSeratOptik.java
+++ PencariRuteKabelSeratOptik.java
@@ -8,6 +8,48 @@ package projectuas_asd;
  *
  * @author advan
  */
-public class PencariRuteKabelSeratOptik {
-    
+public class PencariRuteKabelSeratOptik 
+{
+    static class Vertex 
+    {
+        private String namaKota;
+        private int[][] lokasiRouter;
+        private int jumlahRouter;
+
+        public Vertex(String namaKota) 
+        {
+            this.namaKota = namaKota;
+            this.lokasiRouter = new int[50][2];
+            this.jumlahRouter = 0;
+        }
+
+        public int[][] ambilLokasiRouter() 
+        {
+            return lokasiRouter;
+        }
+
+        public void tambahLokasiRouter(int x, int y) 
+        {
+            if (jumlahRouter < lokasiRouter.length) 
+            {
+                this.lokasiRouter[jumlahRouter][0] = x;
+                this.lokasiRouter[jumlahRouter][1] = y;
+                jumlahRouter++;
+            } 
+            else 
+            {
+                System.out.println("Jumlah lokasi router telah mencapai batas");
+            }
+        }
+
+        public String ambilNamaKota() 
+        {
+            return this.namaKota;
+        }
+
+        public int ambilJumlahRouter() 
+        {
+            return this.jumlahRouter;
+        }
+    }
 }
